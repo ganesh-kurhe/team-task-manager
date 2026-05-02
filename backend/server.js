1a9e39ecc5bfd10936e71beafbbd9543a022b3b2
Ganesh Kurhe <ganeshkurhe512@gmail.com>
2026-05-02 18:50:21 +0530

    Add backend Procfile and prepare app for Railway deployment

--- backend/server.js
+++ backend/server.js
@@ -20,4 +20,5 @@ mongoose.connect(process.env.MONGO_URI)
 .then(()=> console.log("MongoDB Connected"))
 .catch(err => console.log(err));
 
-app.listen(5000, () => console.log("Server running on 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
 });
 
 api.interceptors.request.use((config) => {
@@ -10,4 +10,69 @@ api.interceptors.request.use((config) => {
   return config;
 });
 
-export default api;
+export default api;333
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
